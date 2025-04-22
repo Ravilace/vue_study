470a421523a8ae19be1a8114f4e3879fda6c6002
Ravilace <wujinjun65@gmail.com>
2025-04-22 14:46:02 +0800

    :sparkles: watch ref var and stop watch

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,44 +1,21 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-
-let firstName = ref('张')
-let lastName = ref('三')
-// 这么定义的fullName是一个计算属性，且是只读的
-// let fullName = computed(() => {
-//   return (
-//     firstName.value.slice(0, 1).toUpperCase() +
-//     firstName.value.slice(1) +
-//     '-' +
-//     lastName.value
-//   )
-// })
-let fullName = computed({
-  get() {
-    return (
-      firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
-    )
-  },
-  set(val) {
-    const [str1, str2] = val.split('-')
-    firstName.value = str1
-    lastName.value = str2
-  },
-})
-
-function changeFullName() {
-  fullName.value = 'li-si'
+import { ref, watch } from 'vue'
+let sum =  ref(0)
+function changeSum() {
+  sum.value += 1
 }
+const stopWatch = watch(sum, (newValue, oldValue) => {
+  console.log('sum变化了', newValue, oldValue)
+  if(newValue >= 10) {
+    stopWatch()
+  }
+})
 </script>
 
 <template>
   <div class="person">
-    姓: <input type="text" v-model="firstName" />
-    <br />
-    名: <input type="text" v-model="lastName" />
-    <br />
-    <button @click="changeFullName">将全名更改为li-si</button>
-    <br />
-    全名: <span>{{ fullName }}</span>
+    <h2>{{sum}}</h2>
+    <button @click="changeSum">点我让sum+1</button>
   </div>
 </template>
 

6dac57fad61babb3056c233d18bd3ab9f0225220
Ravilace <wujinjun65@gmail.com>
2025-04-24 10:06:36 +0800

    :boom: route template and style

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,62 @@
-<script setup lang="ts">
-import {ref} from 'vue'
-import Person from './components/Person.vue'
-
-let isShow = ref(true)
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="app">
-    <Person v-if="isShow" />
+  <h2 class="title">Vue路由测试</h2>
+  <!--  导航区-->
+  <div class="navigate">
+    <a href="#" class="active">首页</a>
+    <a href="#">新闻</a>
+    <a href="#">关于</a>
   </div>
+  <!--  展示区-->
+  <div class="main-content">此处以后可能要展示各种组件，到底展示哪个组件，需要看路径</div>
 </template>
-
 <style scoped>
-.app {
-  background-color: #ddd;
-  box-shadow: 0 0 10px;
+.title {
+  text-align: center;
+  word-spacing: 5px;
+  margin: 30px 0;
+  height: 70px;
+  line-height: 70px;
+  background-image: linear-gradient(45deg, gray, white);
   border-radius: 10px;
-  padding: 20px;
+  box-shadow: 0 0 2px;
+  font-size: 30px;
+}
+
+.navigate {
+  display: flex;
+  justify-content: space-around;
+  margin: 0 100px;
+}
+
+.navigate a {
+  display: block;
+  text-align: center;
+  width: 90px;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 10px;
+  background-color: gray;
+  text-decoration: none;
+  color: white;
+  font-size: 18px;
+  letter-spacing: 5px;
+}
+
+.navigate a.active {
+  background-color: #64968e;
+  color: #ffc268;
+  font-weight: 900;
+  text-shadow: 0 0 1px black;
+  font-family: 微软雅黑, serif;
+}
+
+.main-content {
+  margin: 30px auto 0;
+  border-radius: 10px;
+  width: 90%;
+  height: 400px;
+  border: 1px solid;
 }
 </style>

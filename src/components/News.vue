d48f35d66fcfe163f683b024c1843d5af48c2ce3
Ravilace <wujinjun65@gmail.com>
2025-04-24 10:28:21 +0800

    :lipstick: finish components ui

--- src/components/News.vue
+++ src/components/News.vue
@@ -3,9 +3,37 @@
 </script>
 
 <template>
-
+  <div class="news">
+    <ul>
+      <li v-for="i in 4">新闻00{{i + 1}}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
-
+  .news {
+    padding: 0 20px;
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+  }
+  .news ul {
+    margin-top: 30px;
+    list-style: none;
+    padding-left: 10px;
+  }
+  .news li>a {
+    font-size: 18px;
+    line-height: 40px;
+    text-decoration: none;
+    color: #64967E;
+    text-shadow: 0 0 1px rgb(0, 84, 0);
+  }
+  .news-content {
+    width: 70%;
+    height: 90%;
+    border: 1px solid;
+    margin-top: 20px;
+    border-radius: 10px;
+  }
 </style>

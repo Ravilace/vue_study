a2875ec4c1c927d579e8ae037622bf488fd438c4
Ravilace <wujinjun65@gmail.com>
2025-04-25 17:11:26 +0800

    :sparkles: pinia subscribe and use localStorage

--- src/Components/Talk.vue
+++ src/Components/Talk.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-  import {useTalkStore} from "@/store/talk.ts";
-  import {storeToRefs} from "pinia";
-  let {talkList} = storeToRefs(useTalkStore())
-  function getTalk() {
-    useTalkStore().getTalk()
-  }
+import { useTalkStore } from '@/store/talk.ts'
+import { storeToRefs } from 'pinia'
+
+let talkStore = useTalkStore()
+let { talkList } = storeToRefs(talkStore)
+
+function getTalk() {
+  useTalkStore().getTalk()
+}
+
+talkStore.$subscribe((mutate, state) => {
+  console.log('talkStore里面保存的数据发生了变化', mutate, state)
+  localStorage.setItem('talkList', JSON.stringify(state.talkList))
+})
 </script>
 
 <template>
@@ -12,7 +20,7 @@
     <button @click="getTalk">获取一句话</button>
     <ul>
       <li v-for="talk in talkList" :key="talk.id">
-        {{ talk.title}}
+        {{ talk.title }}
       </li>
     </ul>
   </div>
--- src/store/talk.ts
+++ src/store/talk.ts
@@ -19,11 +19,12 @@ export const useTalkStore = defineStore('talk', {
   // 真正存储数据的地方
   state() {
     return {
-      talkList: [
-        { id: '01', title: '测试语句1' },
-        { id: '02', title: '测试语句2' },
-        { id: '03', title: '测试语句3' },
-      ],
+      // talkList: [
+      //   { id: '01', title: '测试语句1' },
+      //   { id: '02', title: '测试语句2' },
+      //   { id: '03', title: '测试语句3' },
+      // ],
+      talkList: JSON.parse(localStorage.getItem('talkList') as string) || []
     }
   },
 })

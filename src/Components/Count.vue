8c417a1829dc8d3f22e9b3c65b07b1760b8a69a3
Ravilace <wujinjun65@gmail.com>
2025-04-25 16:13:52 +0800

    :sparkles: edit pinia data

--- src/Components/Count.vue
+++ src/Components/Count.vue
@@ -3,23 +3,41 @@ import { ref } from 'vue'
 import { useCountStore } from '@/store/count.ts'
 
 const countStore = useCountStore()
-// 以下两种方法都能拿到sum的数据
-// console.log('@@@', countStore.sum)
-// console.log('@@@', countStore.$state.sum)
 let n = ref(1)
+
+function add() {
+  // // 第一种修改方式
+  // countStore.sum += 1
+  // countStore.school = '尚硅谷'
+  // countStore.address = '北京'
+
+  // // 第二种方式
+  // countStore.$patch({
+  //   sum: 888,
+  //   school: '尚硅谷',
+  //   address: '北京',
+  // })
+
+  // 第三种方式，使用pinia那边定义的action
+  countStore.increment(n.value)
+}
+
+function sub() {
+  countStore.sum -= 1
+}
 </script>
 
 <template>
   <div class="count">
-    <h2>当前求和为：{{countStore.sum}}</h2>
-    <!--通过手动指定.number限制类型-->
+    <h2>当前求和为：{{ countStore.sum }}</h2>
+    <h3>欢迎来到: {{ countStore.school }}, 坐落于: {{ countStore.address }}</h3>
     <select v-model.number="n">
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
     </select>
-    <button @click="">加</button>
-    <button @click="">减</button>
+    <button @click="add">加</button>
+    <button @click="sub">减</button>
   </div>
 </template>
 

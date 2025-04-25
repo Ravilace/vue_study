865323baaa45ca4b040849b2f2e0245198b3eaa3
Ravilace <wujinjun65@gmail.com>
2025-04-25 17:16:24 +0800

    :art: compositional style pinia to store data and methods

--- src/store/talk.ts
+++ src/store/talk.ts
@@ -1,10 +1,38 @@
 import { defineStore } from 'pinia'
-import axios from "axios";
-import {nanoid} from "nanoid";
+import axios from 'axios'
+import { nanoid } from 'nanoid'
+import {reactive} from "vue";
 
-export const useTalkStore = defineStore('talk', {
-  actions: {
-    async getTalk() {
+// 选项式
+// export const useTalkStore = defineStore('talk', {
+//   actions: {
+//     async getTalk() {
+//       // 发请求，下面这行的写法是：连续解构赋值+重命名
+//       let {data: {content: title}} = await axios.get('/api/qinghua')
+//       // 把请求回来的字符串，包装成一个对象
+//       let obj = {
+//         id: nanoid(),
+//         title
+//       }
+//       console.log(obj)
+//       this.talkList.unshift(obj)
+//     }
+//   },
+//   // 真正存储数据的地方
+//   state() {
+//     return {
+//       talkList: JSON.parse(localStorage.getItem('talkList') as string) || []
+//     }
+//   },
+// })
+
+export const useTalkStore = defineStore('talk', () => {
+
+    const talkList = reactive(
+        JSON.parse(localStorage.getItem('talkList') as string) || []
+    )
+
+    async function getTalk() {
       // 发请求，下面这行的写法是：连续解构赋值+重命名
       let {data: {content: title}} = await axios.get('/api/qinghua')
       // 把请求回来的字符串，包装成一个对象
@@ -13,18 +41,7 @@ export const useTalkStore = defineStore('talk', {
         title
       }
       console.log(obj)
-      this.talkList.unshift(obj)
+      talkList.unshift(obj)
     }
-  },
-  // 真正存储数据的地方
-  state() {
-    return {
-      // talkList: [
-      //   { id: '01', title: '测试语句1' },
-      //   { id: '02', title: '测试语句2' },
-      //   { id: '03', title: '测试语句3' },
-      // ],
-      talkList: JSON.parse(localStorage.getItem('talkList') as string) || []
-    }
-  },
+    return {talkList, getTalk}
 })

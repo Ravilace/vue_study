392a1c3aa9ba6251d71e82ce3ec8c07334967010
Ravilace <wujinjun65@gmail.com>
2025-05-06 15:01:39 +0800

    :sparkles: props between father and son

--- src/pages/01_props/Child.vue
+++ src/pages/01_props/Child.vue
@@ -1,8 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let toy = ref('奥特曼')
+let props = defineProps(['car', 'sendToy'])
+</script>
 
 <template>
   <div class="child">
     <h3>子组件</h3>
+    <h4>玩具：{{ toy }}</h4>
+    <h4>父亲给的车：{{ car }}</h4>
+    <button @click="sendToy(toy)">把玩具给父亲</button>
   </div>
 </template>
 
--- src/pages/01_props/Father.vue
+++ src/pages/01_props/Father.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
 import Child from './Child.vue'
-import {ref} from 'vue'
+import { ref } from 'vue'
 // 数据
-let car  = ref('奔驰')
+let car = ref('奔驰')
+let toy = ref('')
+
+function getToy(value: string) {
+  toy.value = value
+}
 </script>
 
 <template>
   <div class="father">
     <h3>父组件</h3>
-    <Child />
+    <h4>汽车：{{ car }}</h4>
+    <h4 v-show="toy">子给的玩具：{{ toy }}</h4>
+    <Child :car="car" :sendToy="getToy" />
   </div>
 </template>
 
 <style scoped>
-  .father {
-    background-color: rgb(165, 164, 164);
-    padding: 20px;
-    border-radius: 10px;
-  }
+.father {
+  background-color: rgb(165, 164, 164);
+  padding: 20px;
+  border-radius: 10px;
+}
 </style>

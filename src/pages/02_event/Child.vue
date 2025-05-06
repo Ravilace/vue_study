6b4a957b4f0f966a3d2d6d6dda5cc5483452727a
Ravilace <wujinjun65@gmail.com>
2025-05-06 15:24:43 +0800

    :construction: prepare for event study

---
+++ src/pages/02_event/Child.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div class="child">
+  </div>
+</template>
+
+<style scoped>
+.child {
+  background-color: skyblue;
+  padding: 10px;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+}
+</style>
---
+++ src/pages/02_event/Father.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Child from './Child.vue'
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div class="father">
+
+  </div>
+</template>
+
+<style scoped>
+.father {
+  background-color: rgb(165, 164, 164);
+  padding: 20px;
+  border-radius: 10px;
+}
+</style>

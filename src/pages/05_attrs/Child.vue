8af56d049d151703ab09f8b41522741c2f575a32
Ravilace <wujinjun65@gmail.com>
2025-05-08 10:12:34 +0800

    :sparkles: pass function to child

--- src/pages/05_attrs/Child.vue
+++ src/pages/05_attrs/Child.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-defineProps(['a', 'b', 'c', 'x', 'y'])
+defineProps(['a', 'b', 'c', 'x', 'y', 'updateFunc'])
 </script>
 
 <template>
   <div class="child">
     <h3 class="child">子组件</h3>
     <h4>a: {{ a }}, b: {{ b }}, c: {{ c }}, x: {{ x }}, y:{{ y }}</h4>
+    <button @click="updateFunc(6)">给a加6</button>
   </div>
 </template>
 
--- src/pages/05_attrs/GrandFather.vue
+++ src/pages/05_attrs/GrandFather.vue
@@ -5,13 +5,17 @@
   const a = ref(0)
   const b = ref(1)
   const c = ref(2)
+
+  function updateA(value: number) {
+    a.value += value
+  }
 </script>
 
 <template>
   <div class="grandfather" >
     <h3>爷组件</h3>
     <h4>a: {{a}}, b: {{b}}, c: {{c}}, </h4>
-    <Father :a="a" :b="b" :c="c" v-bind="{x: 3, y: 4}"/>
+    <Father :a="a" :b="b" :c="c" v-bind="{x: 3, y: 4}" :updateFunc="updateA"/>
   </div>
 </template>
 

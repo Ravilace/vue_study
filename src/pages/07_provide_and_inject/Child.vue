22ea8d0576c7470980843607df6e0b1ae5201f33
Ravilace <wujinjun65@gmail.com>
2025-05-09 09:46:34 +0800

    :sparkles: finish provide and inject

--- src/pages/07_provide_and_inject/Child.vue
+++ src/pages/07_provide_and_inject/Child.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-defineProps(['a', 'b', 'c', 'x', 'y', 'updateFunc'])
+import { inject } from 'vue'
+
+let { money, spendMoney } = inject('moneyContext', {
+  money: '默认金额',
+  spendMoney: (x: number) => {},
+})
+let car = inject('car', { name: '未知车辆', price: 0 })
 </script>
 
 <template>
   <div class="child">
     <h3 class="child">子组件</h3>
+    <h4>孙子有{{ money }}万元</h4>
+    <h4>孙子继承的车是{{ car.name }}，价值{{ car.price }}万元</h4>
+    <button @click="spendMoney(6)">花爷爷的钱</button>
   </div>
 </template>
 
--- src/pages/07_provide_and_inject/GrandFather.vue
+++ src/pages/07_provide_and_inject/GrandFather.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+import { ref, reactive, provide } from 'vue'
   import Father from './Father.vue'
+
+  let money = ref(100)
+  let car = reactive({
+    'name': '奔驰',
+    'price': 200,
+  })
+  function spendMoney(value:number) {
+    money.value -= value
+  }
+
+  provide('moneyContext', {'money': money, 'spendMoney': spendMoney})
+  provide('car', car)
 </script>
 
 <template>
   <div class="grandfather" >
     <h3>爷组件</h3>
+    <h4>爷爷有{{money}}万元</h4>
+    <h4>爷爷的车是{{car.name}}，价值{{car.price}}万元</h4>
     <Father />
   </div>
 </template>
